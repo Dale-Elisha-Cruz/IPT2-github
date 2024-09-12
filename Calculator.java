0b0ff61d7cde0c6ed7a5847029fc1b5295ad15c6
Dale-Elisha-Cruz <cruzdaleelisha.01@gmail.com>
2024-09-12 14:28:06 +0800

    Add Addition Method

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,14 @@
 public class Calculator{
 
+    public static int add(int a, int b){
+        return a + b;
+    }
+
 
 
 
     public static void main (String[] args){
         System.out.println("BASIC CALCULATOR");
+        System.out.println("Addition: " +add(5, 3));
     }
 }

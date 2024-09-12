dea26826d9a9eee8a936fcb4c00d2c993bd13be0
Dale-Elisha-Cruz <cruzdaleelisha.01@gmail.com>
2024-09-12 14:50:08 +0800

    Add Division Method

--- Calculator.java
+++ Calculator.java
@@ -12,13 +12,15 @@ public class Calculator{
         return a * b;
     }
 
-
-
+    public static int divide(int a, int b){
+        return a / b;
+    }
 
     public static void main (String[] args){
         System.out.println("BASIC CALCULATOR");
         System.out.println("Addition: " +add(5, 3));
         System.out.println("Subtraction: " +subtract(4, 2));
         System.out.println("Multiplication: " +multiply(6, 2));
+        System.out.println("Division: " +divide(3, 3));
     }
 }
